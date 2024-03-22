5b11b6a6c6c6fbf3e51a9c3edac5f5d6bb4ee15e
Haden Bennett <hadenbennett444@gmail.com>
2024-03-22 13:53:50 -0500

    Papa is mad

---
+++ duck/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+import AboutView from './views/AboutView.vue'
+</script>
+
+<template>
+
+  <HomeView id="home"></HomeView>
+  <AboutView id="about"></AboutView>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ duck/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+
+  <main>
+    <header>
+      <nav>
+        <router-link :to="{hash: '#home'}">Home</router-link>
+        <router-link :to="{hash: '#about'}">About</router-link>
+      </nav>
+  </header>
+    This is the home page
+  </main>
+</template>
+
+<style scoped>
+main{
+  width: 100%;
+  background-color: aqua;
+  min-height: 100vh;
+}
+</style>
